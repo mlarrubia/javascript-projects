d2dfe596a2d26f27b39bf92e9687cacba5b77284
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-15 18:28:50 -0400

    Add & Delete Transactions

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -1,11 +1,11 @@
 
-const balance = document.getElementById('balance')
-const money_plus = document.getElementById('money-plus')
-const money_minus = document.getElementById('money-minus')
-const list = document.getElementById('list')
-const form = document.getElementById('form')
-const text = document.getElementById('text')
-const amount = document.getElementById('amount')
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
 
 const dummyTransactions = [
   { id: 1, text: 'Flower', amount: -20 },
@@ -18,21 +18,51 @@ const dummyTransactions = [
 let transactions = dummyTransactions;
 
 
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
 
-// Add tranactions to DOM List
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert("Please add text")
+  }
+  else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    }
+    transactions.push(transaction);
 
-function addTransactionDOM(tranaction) {
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    text.value = "";
+    amount.value = "";
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+
+
+// Add transactions to DOM List
+
+function addTransactionDOM(transaction) {
   // Get sign
-  const sign = tranaction.amount < 0 ? '-' : '+';
+  const sign = transaction.amount < 0 ? '-' : '+';
 
   const item = document.createElement('li');
 
   // Add Class based on value
-  item.classList.add(tranaction.amount < 0 ? "minus" : "plus");
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
 
   item.innerHTML = `
-    ${tranaction.text} <span>${sign}${Math.abs(tranaction.amount)}</span>
-    <button class="delete-btn">X</button>
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">X</button>
   `;
 
   list.appendChild(item);
@@ -72,16 +102,13 @@ function updateValues() {
 
 
 
+// Remove Transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id
+    !== id);
 
-
-
-
-
-
-
-
-
-
+  init();
+}
 
 
 
@@ -99,6 +126,10 @@ function init() {
 
 init();
 
+
+
+
+form.addEventListener('submit', addTransaction);
 
 
 
