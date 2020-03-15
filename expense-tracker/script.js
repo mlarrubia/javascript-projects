c80bf9bde4084e1d4b390660d5edba371994b8d2
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-15 17:31:10 -0400

    Show Transaction Items

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -1,1 +1,89 @@
 
+const balance = document.getElementById('balance')
+const money_plus = document.getElementById('money-plus')
+const money_minus = document.getElementById('money-minus')
+const list = document.getElementById('list')
+const form = document.getElementById('form')
+const text = document.getElementById('text')
+const amount = document.getElementById('amount')
+
+const dummyTransactions = [
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: "Salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 }
+];
+
+
+let transactio
+
+ns = dummyTransactions;
+
+
+
+// Add tranactions to DOM List
+
+function addTransactionDOM(tranaction) {
+  // Get sign
+  const sign = tranaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+
+  // Add Class based on value
+  item.classList.add(tranaction.amount < 0 ? "minus" : "plus");
+
+  item.innerHTML = `
+    ${tranaction.text} <span>${sign}${Math.abs(tranaction.amount)}</span>
+    <button class="delete-btn">X</button>
+  `;
+
+  list.appendChild(item);
+
+
+}
+
+// Init app
+
+function init() {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+}
+
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
