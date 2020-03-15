d6175f3b03b2df222caf48b0ca208dac46820a99
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-15 18:46:37 -0400

    Persist to Local Storage

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -7,15 +7,17 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const dummyTransactions = [
-  { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 }
-];
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: "Salary", amount: 300 },
+//   { id: 3, text: "Book", amount: -10 },
+//   { id: 4, text: "Camera", amount: 150 }
+// ];
+
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
 
 
-let transactions = dummyTransactions;
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 
 // Add transaction
@@ -36,6 +38,8 @@ function addTransaction(e) {
     addTransactionDOM(transaction);
 
     updateValues();
+
+    updateLocalStorage();
 
     text.value = "";
     amount.value = "";
@@ -107,9 +111,17 @@ function removeTransaction(id) {
   transactions = transactions.filter(transaction => transaction.id
     !== id);
 
+  updateLocalStorage();
+
   init();
 }
 
+
+
+// update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
 
 
 
