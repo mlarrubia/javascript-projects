ccb3bf34960e820694e4f6af5c97cac18ee8f1c2
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-15 23:19:28 -0400

    Adding the Timer

--- Typing Game/script.js
+++ Typing Game/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('setting-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -41,6 +41,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Focus on text input on start
+text.focus();
+
+// Start Counting down
+const timeInterval = setInterval(updateTime, 1000);
+
 
 // Generate random word from array
 function getRandomWord() {
@@ -59,6 +65,32 @@ function updateScore() {
     scoreEl.innerHTML = score;
 }
 
+
+function updateTime() {
+    time--;
+    timeEl.innerHTML = time + 's';
+
+    if (time === 0) {
+        clearInterval(timeInterval);
+
+        // end game
+        gameOver();
+    }
+}
+
+// Game over, show end screen
+function gameOver() {
+    endgameEl.innerHTML = `
+        <h1>Time ran out</h1>
+        <p>Your final score is ${score}</p>
+        <button onclick="location.reload()">Play Again</button>
+    `;
+
+    endgameEl.style.display = "flex";
+
+}
+
+
 addWordToDOM();
 
 
@@ -71,5 +103,8 @@ text.addEventListener('input', e => {
         updateScore();
 
         e.target.value = '';
+
+        time += 2;
+        updateTime();
     }
 })
