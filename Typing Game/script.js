aef5b7cb28e8f172ef625424b86ecc2e522eae71
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-15 21:25:19 -0400

    Word Match & Score

---
+++ Typing Game/script.js
@@ -0,0 +1,75 @@
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game');
+const settingsBtn = document.getElementById('setting-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
+
+
+// List of words for game
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'car',
+    'baseball',
+    'basketball',
+    'kite',
+    'train',
+    'duck',
+    'alphabet',
+    'google',
+    'apple',
+    'tesla',
+    'loving'
+];
+
+
+// Init word
+let randomWord;
+
+// Init Score
+let score = 0;
+
+// Init time
+let time = 10;
+
+
+// Generate random word from array
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+// Add word to DOM
+function addWordToDOM() {
+    randomWord = getRandomWord();
+    word.innerHTML = randomWord;
+}
+
+// Update Score
+function updateScore() {
+    score++;
+    scoreEl.innerHTML = score;
+}
+
+addWordToDOM();
+
+
+// Event Listeners
+
+text.addEventListener('input', e => {
+    const insertedText = e.target.value;
+    if (insertedText === randomWord) {
+        addWordToDOM();
+        updateScore();
+
+        e.target.value = '';
+    }
+})
