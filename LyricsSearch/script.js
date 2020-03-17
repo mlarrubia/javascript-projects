b36ec5fb2100caf3bca8a89cd01f180be6d63be3
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-17 15:31:30 -0400

    Fetching Songs and Artist

---
+++ LyricsSearch/script.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const result = document.getElementById('result')
+const more = document.getElementById('more')
+
+const apiURL = 'https://api.lyrics.ovh';
+
+// Search by Song or Artist
+async function searchSong(term) {
+    // Fetch API
+    // fetch(`${apiURL}/suggest/${term}`)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data));
+
+    // Async await API
+    const res = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await res.json();
+
+    showData(data);
+}
+
+
+// Show song and artist in DOM
+function showData(data) {
+
+}
+
+
+// Event Listener
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const searchTerm = search.value.trim();
+
+
+    if (!searchTerm) {
+        alert("Please type in a search term")
+    }
+    else {
+        searchSong(searchTerm);
+    }
+
+})
