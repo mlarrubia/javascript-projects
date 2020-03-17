8fe43159e59cf1e713ad4bd4b59ed75c65f30324
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-17 17:55:06 -0400

    Get & Display Song Lyrics

--- LyricsSearch/script.js
+++ LyricsSearch/script.js
@@ -63,6 +63,22 @@ async function getMoreSongs(url) {
 }
 
 
+// Get Lyrics and song
+async function getLyrics(artist, songTitle) {
+
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - 
+    ${songTitle}</h2>
+    <span>${lyrics}</span>`;
+
+    more.innerHTML = ''
+}
+
+
 // Event Listener
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -77,4 +93,17 @@ form.addEventListener('submit', e => {
         searchSong(searchTerm);
     }
 
+})
+
+
+// Get Lyrics button click
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+    if (clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute("data-artist")
+        const songTitle = clickedEl.getAttribute("data-songtitle")
+
+        getLyrics(artist, songTitle);
+
+    }
 })
