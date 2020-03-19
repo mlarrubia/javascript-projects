20a16d7347a6d80017cc82ef073ffb2e7e6c82c4
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-18 23:46:55 -0400

    Output Users - forEach() & DOM Methods

--- DOM Array Methods/script.js
+++ DOM Array Methods/script.js
@@ -1,4 +1,6 @@
 const main = document.getElementById('main');
+
+
 const addUserBtn = document.getElementById('add-user');
 const doubleBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionaires');
@@ -9,9 +11,6 @@ const calculateWealthBtn = document.getElementById('calculate-wealth');
 let data = [];
 
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
 
 //  Fetch random user and add money
 
@@ -26,9 +25,7 @@ async function getRandomUser() {
         name: `${user.name.first} ${user.name.last}`,
         money: Math.floor(Math.random() * 1000000)
     }
-
-    console.log(newUser)
-
+    addData(newUser);
 }
 
 
@@ -36,4 +33,32 @@ async function getRandomUser() {
 
 function addData(obj) {
     data.push(obj);
+
+    updateDOM();
 }
+
+// Update DOM
+function updateDOM(provideData = data) {
+    // Clear the main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    provideData.forEach(item => {
+        console.log(item)
+        const element = document.createElement("div");
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`
+        main.appendChild(element);
+    })
+}
+
+
+// Format number as Money
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+
+
+// Event Listeners
+
+addUserBtn.addEventListener('click', getRandomUser);
