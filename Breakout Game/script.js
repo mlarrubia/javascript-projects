02f4f94c7841ad601a32818f84091b1250f79a27
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-23 11:42:38 -0400

    Move Paddle

--- Breakout Game/script.js
+++ Breakout Game/script.js
@@ -54,8 +54,6 @@ for (let i = 0; i < brickRowCount; i++) {
     }
 }
 
-console.log(bricks);
-
 // Draw ball on canvas
 function drawBall() {
     ctx.beginPath();
@@ -87,17 +85,30 @@ function drawBricks() {
     })
 }
 
+// Move paddle on canvas
+function movePaddle() {
+    paddle.x += paddle.dx;
 
+    // Wall dectection
+    if (paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w;
+    }
+
+    if (paddle.x < 0) {
+        paddle.x = 0;
+    }
+}
 
 // Draw Everything
 function draw() {
+    // Clear Canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     drawBall();
     drawPaddle();
     drawScore();
     drawBricks();
 }
-
-
 
 // Draw score on canvas
 function drawScore() {
@@ -105,7 +116,36 @@ function drawScore() {
     ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
 }
 
-draw();
+// Update Canvas drawing and animation
+function update() {
+    movePaddle();
+    // Draw Everything
+    draw();
+
+    requestAnimationFrame(update);
+}
+
+update();
+
+// Keydown event
+function keyDown(e) {
+    if (e.key === "Right" || e.key === 'ArrowRight') {
+        paddle.dx = paddle.speed;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        paddle.dx = -paddle.speed;
+    }
+}
+
+// KeyUp event 
+function keyUp(e) {
+    if (e.key === "Right" || e.key === "ArrowRight" || e.key === "Left" || e.key === "ArrowLeft") {
+        paddle.dx = 0;
+    }
+}
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 // Rules and close Event Handler
 rulesBtn.addEventListener('click', () => {
