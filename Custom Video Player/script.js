b0de48543263866f9601a75262c49259c6893ff1
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-27 01:41:46 -0400

    Play, Pause & Stop

---
+++ Custom Video Player/script.js
@@ -0,0 +1,52 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Play and Pause video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play()
+    } else {
+        video.pause();
+    }
+}
+
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    }
+    else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
+
+function updateProgress() {
+
+}
+
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+function setVideoProgress() {
+
+}
+
+
+
+
+
+
+
+// Event Listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('click', setVideoProgress);
