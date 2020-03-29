ee034f211c65df2c0fc5b46f2babb5692a6809cd
Matthew Larrubia <mlarrubia@columbushs.com>
2020-03-28 20:27:23 -0400

    Load, Play & Pause Song

---
+++ Music Player/script.js
@@ -0,0 +1,73 @@
+const musicContainer = document.getElementById('music-container');
+
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+// Song titles
+const songs = ["hey", "summer", "ukulele"];
+
+// Keep track of songs
+
+let songIndex = 1;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpg`;
+
+    // if (musicContainer.classList.contains('play')) {
+    //     audio.play();
+    // }
+}
+
+// Play Song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fa').classList.remove('fa-play')
+    playBtn.querySelector('i.fa').classList.add('fa-pause')
+
+    audio.play();
+}
+
+
+// Pause Song 
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fa').classList.remove('fa-pause')
+    playBtn.querySelector('i.fa').classList.add('fa-play')
+
+    audio.pause();
+}
+
+
+
+
+
+
+// Event Listener
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+})
+
+// nextBtn.addEventListener('click', () => {
+//     songIndex++;
+//     songIndex %= 3;
+//     loadSong(songs[songIndex]);
+// })
