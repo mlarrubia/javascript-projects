a6d1fb3700eb5a0bff0c1b0019d2252241ac3366
Matthew Larrubia <mlarrubia@columbushs.com>
2020-04-02 21:49:02 -0400

    Insert List into DOM

---
+++ Sortable List/script.js
@@ -0,0 +1,48 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+    'Jeff Bezos',
+    'Bill Gates',
+    'Warren Buffett',
+    'Bernard Arnault',
+    'Carlos Slim Helu',
+    'Amancio Ortega',
+    'Larry Ellison',
+    'Mark Zuckerberg',
+    'Michael Bloomberg',
+    'Larry Page'
+];
+
+// Store listItems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+
+//  Insert list items into DOM
+function createList() {
+    [...richestPeople]
+        .forEach((person, index) => {
+            const listItem = document.createElement('li');
+            console.log(listItem);
+            listItem.setAttribute('data-index', index);
+
+            listItem.innerHTML = `
+                <span class="number">${index + 1}</span>
+                <div class="draggable" draggable="true">
+                    <p class="person-name">${person}</p>
+                    <i class="fa fa-grip-lines"></i>
+                </div>
+            `;
+
+            listItems.push(listItem);
+
+            draggable_list.appendChild(listItem);
+        })
+
+
+
+}
