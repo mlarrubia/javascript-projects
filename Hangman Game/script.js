ea89964ecad4f8c014bbeb6a89145a97200f6fb2
Matthew Larrubia <mlarrubia@columbushs.com>
2020-04-09 15:27:31 -0400

    Wrong Letters & Play Again

--- Hangman Game/script.js
+++ Hangman Game/script.js
@@ -1,11 +1,11 @@
 const wordEl = document.getElementById('word')
 const wrongLettersEl = document.getElementById('wrong-letters')
-const playAgainBtn = document.getElementById('play-again')
+const playAgainBtn = document.getElementById('play-button')
 const popup = document.getElementById('popup-container')
 const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 
-const figureParts = document.querySelectorAll('figure-part');
+const figureParts = document.querySelectorAll('.figure-part');
 
 const words = ["application", "programming", "interface", "wizard"]
 
@@ -36,7 +36,31 @@ function displayWord() {
 
 // Update teh wrong letters
 function updateWrongLettersEl() {
-    console.log("update wrong");
+
+    // Display wrong letters
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    // Display Parts
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+
+        if (index < errors) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+    })
+
+    // Check if lost
+    if (wrongLetters.length == figureParts.length) {
+        finalMessage.innerText = "Unfortunately you lost... :("
+        popup.style.display = 'flex';
+    }
+
+
 }
 
 // Show notification
@@ -47,6 +71,21 @@ function showNotification() {
         notification.classList.remove('show');
     }, 2000)
 }
+
+// Restart Game
+playAgainBtn.addEventListener('click', () => {
+    // Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    displayWord();
+
+    updateWrongLettersEl();
+
+    popup.style.display = 'none';
+})
 
 // Keydown letter press
 window.addEventListener('keydown', e => {
